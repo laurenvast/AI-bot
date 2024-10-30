54c338669ed88805d974be52cde40ef6276327ad
Lauren Wang <heylaurenwang@gmail.com>
2024-10-30 18:55:36 -0400

    add site

---
+++ ai_context/config.js
@@ -0,0 +1,7 @@
+// config.js
+const CONFIG = {
+    // ANTHROPIC_API_KEY: 'sk-ant-api03-s5lVvN66nZ4vAs7Rt9lpm'+'fmY05QlySwAF9jKbQRnoyW_om4Y6P6E7EX1oKMn'+'n887taRTjzkzRgn3hEP0Gzasyw-wcLo5AAA'
+    ANTHROPIC_API_KEY: 'n887taRTjzkzRgn3hEP0Gzasyw-wcLo5AAA'
+};
+
+export default CONFIG;
